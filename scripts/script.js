8f795700c5bff5fec1157db73e68196ddf99c6d7
wedfor <we029d@gmail.com>
2023-10-10 19:51:01 +0300

    Add modal window

---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+
+
+// const body = document.querySelector('body');
+// const navbar = document.querySelector('nav');
+// const works = document.querySelector('.works');
+
+// const btn = document.createElement('button');
+// btn.textContent = 'add work';
+// navbar.appendChild(btn);
+
+// function work(linkWork){
+//     const srcWork = linkWork;
+//     const work = document.createElement('img');
+    
+//     work.setAttribute('src', srcWork);
+//     console.log(typeof work);
+//     return work;
+    
+// }
+
+// btn.addEventListener('click', () => 
+// works.appendChild(work('image/sky.jpg')));
+
+
+
+
+
+
+
+
+function viewPhoto(img){
+    img = img.cloneNode()
+    modal.style.display = "flex";
+    modal.appendChild(img);
+    
+    function closePhoto(img){
+        modal.removeChild(img);
+        modal.style.display = 'none';
+        console.log('close');
+    }
+    if (modal.style.display !=='none'){
+            console.log('open');
+            console.log(img);
+            img.removeEventListener('click',() => viewPhoto(currentValue));
+            img.addEventListener('click', () =>closePhoto(img));
+        }
+}
+
+
+
+const modal = document.querySelector('.modal');
+const img = document.querySelectorAll('img');
+
+
+img.forEach(function(currentValue){
+    currentValue.addEventListener('click', () => viewPhoto(currentValue))
+})
+
+
+
